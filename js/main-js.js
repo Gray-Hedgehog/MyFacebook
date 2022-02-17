c8edd02c49a6c08b0668db15fef91a4e217e6bf9
Gray-Hedgehog <hedgehog5349@gmail.com>
2022-02-17 22:29:23 +0300

    mobile

--- js/main-js.js
+++ js/main-js.js
@@ -6,29 +6,10 @@ function myFunction() {
 
 }
 
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//     if (!event.target.matches('.drop')) {
-//
-//         var dropdowns = document.getElementsByClassName("down-icons");
-//         var i = 1;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//
-//         // if (openDropdown.classList.contains('close')) {
-//         //     openDropdown.classList.remove('close');
-//         // }
-//     }
-// }
+
 var dropdowns = document.getElementsByClassName("down-icons");
 var i = 1;
 myClose.onfocus = function () {
-    // if (this.classList.contains())
-    // openDropdown.classList.contains('show');
     myClose.classList.contains('close');
 }
 
