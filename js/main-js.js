75c31aeb59c218fb69d7343adfee15c8b602e623
Gray-Hedgehog <hedgehog5349@gmail.com>
2022-03-22 23:16:16 +0300

    new burger

--- js/main-js.js
+++ js/main-js.js
@@ -1,37 +1,50 @@
 // burger
-function myFunction() {
-    document.getElementById("myClose").classList.toggle("close");
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-
-var dropdowns = document.getElementsByClassName("down-icons");
-var i = 1;
-myClose.onfocus = function () {
-    myClose.classList.contains('close');
-}
-
-myClose.onblur = function () {
-    if (myClose.classList.contains('close')) {
-        myClose.classList.remove('close');
-    }
-
-    for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
-    }
-}
 
 const menuIcon = document.querySelector('.menu-icon')
-if (menuIcon) {
-    const menuBody = document.querySelector('.menu');
-    menuIcon.addEventListener("click", function (e) {
-        document.body.classList.toggle('_lock');
-        menuBody.classList.toggle('_active');
-    });
+const menu = document.querySelector('.menu')
+const body = document.querySelector('body')
+function burger(event) {
+    menu.classList.toggle('_active')
+    body.classList.toggle('_lock')
 }
+
+menuIcon.addEventListener('click', burger)
+
+// оставил для сравнения
+
+// function myFunction() {
+//     document.getElementById("myClose").classList.toggle("close");
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+//
+//
+// var dropdowns = document.getElementsByClassName("down-icons");
+// var i = 1;
+// myClose.onfocus = function () {
+//     myClose.classList.contains('close');
+// }
+//
+// myClose.onblur = function () {
+//     if (myClose.classList.contains('close')) {
+//         myClose.classList.remove('close');
+//     }
+//
+//     for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//             openDropdown.classList.remove('show');
+//         }
+//     }
+// }
+//
+// const menuIcon = document.querySelector('.menu-icon')
+// if (menuIcon) {
+//     const menuBody = document.querySelector('.menu');
+//     menuIcon.addEventListener("click", function (e) {
+//         document.body.classList.toggle('_lock');
+//         menuBody.classList.toggle('_active');
+//     });
+// }
 
 // handle like
 
