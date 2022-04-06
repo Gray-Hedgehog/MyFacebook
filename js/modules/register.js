f1ab24ec71d79be24bb2d1a10fc46cdafd17dc4e
Gray-Hedgehog <hedgehog5349@gmail.com>
2022-04-06 20:26:47 +0300

    register

--- js/modules/register.js
+++ js/modules/register.js
@@ -1,13 +1,14 @@
-
-const name = document.getElementById("name");
-const lastName = document.getElementById("lastName");
-const phoneNumber = document.getElementById("phoneNumber");
-const email = document.getElementById("mail");
-
-email.addEventListener("input", function (event) {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("I am expecting an e-mail address!");
-    } else {
-        email.setCustomValidity("");
-    }
-});
+//
+// const name = document.getElementById("name");
+// const lastName = document.getElementById("lastName");
+// const phoneNumber = document.getElementById("phoneNumber");
+// const email = document.getElementById("mail");
+//
+// email.addEventListener("input", function (event) {
+//     console.log('email')
+//     if (email.validity.typeMismatch) {
+//         email.setCustomValidity("I am expecting an e-mail address!");
+//     } else {
+//         email.setCustomValidity("");
+//     }
+// });
